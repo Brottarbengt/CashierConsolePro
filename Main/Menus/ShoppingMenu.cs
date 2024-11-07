4f5ea0e3fa212045739ab4b78fa988d7b0011c15
Karl Westergren <karlvestergren@gmail.com>
2024-11-07 19:13:13 +0100

    One small step for man, one giant leap for Karl pedal.

--- Main/Menus/ShoppingMenu.cs
+++ Main/Menus/ShoppingMenu.cs
@@ -10,36 +10,42 @@ namespace CashierConsolePro.Menus
     {
         public static void CashierConsole()
         {
+            
             ShoppingCart cart = new ShoppingCart();
-            Console.WriteLine("|Cashier console|");
 
-            foreach (var item in cart.GetAllProducts())
+            while (true)
             {
-                Console.WriteLine($"{item.Product.Name} , {item.Amount} * {item.Product.Price} = {item.Total}");
-            }
+                Console.Clear();
+                Console.WriteLine("|Cashier console|");
 
-            Console.WriteLine("Commands: \n <product code> <amount> Or just type 'PAY' for checkout");
+                foreach (var item in cart.GetAllProducts())
+                {
+                    Console.WriteLine($"{item.Product.Name} , {item.Amount} * {item.Product.Price} = {item.Total}");
+                }
 
-            var commands = Console.ReadLine().Split(" ");
+                Console.WriteLine("\nCommands: \n <product code> <amount> Or type 'PAY' for checkout");
 
-            if (commands.Length == 1 && InputValidator.IsPayCommand(commands[0]))
-            {
-                var receipt = cart.CheckOut();
-                receipt.CreateReceipt();
-                receipt.DisplayReceipt();
+                var commands = Console.ReadLine().Split(" ");
+
+                if (commands.Length == 1 && InputValidator.IsPayCommand(commands[0]))
+                {
+                    var receipt = cart.CheckOut();
+                    receipt.CreateReceipt();
+                    receipt.DisplayReceipt();
+                    break;  
+                }
+
+                else if (commands.Length == 2 && InputValidator.IsValidProductCode(commands[0]) && InputValidator.IsValidAmount(commands[1]))
+                {
+                    var productStore = ProductStore.Instance();
+                    var product = productStore.FindProduct(commands[0]);
+                    var amount = int.Parse(commands[1]);
+                    var cartItem = new CartItem(amount, product);
+                    cart.AddProductToCart(cartItem);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid product code and amount, or type 'PAY' to checkout.");
+                }
             }
-            else if (commands.Length == 2 && InputValidator.IsValidProductCode(commands[0]) && InputValidator.IsValidAmount(commands[1]))
-            {
-                var productStore = ProductStore.Instance();
-                var product = productStore.FindProduct(commands[0]);
-                var amount = int.Parse(commands[1]);
-                var cartItem = new CartItem(amount, product);
-                cart.AddProductToCart(cartItem);
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter a valid product code and amount, or type 'PAY' to checkout.");
-            }
-        }
-    }
 }
